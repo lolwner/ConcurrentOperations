7937820199c2e00e794f181e0031c21f14bd8949
Anuitex <modelerproject@outlook.com>
2018-12-26 12:34:18 +0200

    Add project files.

---
+++ ConcurrentOperations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConcurrentOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
